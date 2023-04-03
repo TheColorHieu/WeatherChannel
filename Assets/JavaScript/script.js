cb55d0c6456a32f3911bc7fad2fca1d2e578068b
Hieu Tran <hieuthanhtran98@yahoo.com>
2023-04-03 00:16:27 -0700

    added the 5 day forecast function

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -87,8 +87,39 @@ function weatherFunction(searchTerm){
         $("#today .card-body").append(uvIndex.append(btn));
         console.log(data);
 
+        //creating the weatherForecast function
+        function weatherForecast(searchTerm){
+            $.ajax({
+                type: "GET",
+                url : "https://api.openweathermap.org/data/2.5/forecast?q=" + searchTerm + "&appid=e9a2983c2714f2ea8438474602154f62=imperial",
+            }).then(function (data) {
+                console.log(data);
+                $("#forecast").html("<h4 class=\"mt-3\">5-Day Forecast:</h4>").append("<div class=\"row\">");
+
+                //creating the loop for the 5 days to pull the data 
+            for (var i =0; i < data.list.length; i++){
+                if(data.list[i]).dt_txt.indexOf("15:00:00") !== -1){
+                    var titleFive = $("<h3>").addClass("card-title").text(new Date(data.list[i].dt_txt).toLocaleDateString());
+                    var imgFive = $("<img>").attr("src", "https://openweathermap.org/img/w/" + data.list[i].weather[0].icon + ".png");
+                    var colFive = $("<div>").addClass("col-md-2.5");
+                    var cardFive = $("<div>").addClass("card bg-primary text-white");
+                    var cardBodyFive = $("<div>").addClass("card-body p-2");
+                    var humidFive = $("<p>").addClass("card-text").text("Humidity: " + data.list[i].main.humidity + "%");
+                    var tempFive = $("<p>").addClass("card-text").text("Temperature: " + data.list[i].main.temp + " °F");
+
+                    //mering together and appending it 
+                    colFive.append(cardFive.append(cardBodyFive.append(tittleFive, imgFive, tempFive, humidFive)));
+                    //append card, body to card and other elements to the body
+                    $("#forecast .row").append(colFive);
+                }
+            }
+            });
+
+        }
+
+
     });
-}
+
 
 
 })
